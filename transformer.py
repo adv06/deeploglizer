fdd5a94db86db1a7b71ee658c311907a8ab773f4
unknown <adveysnet@gmail.com>
2025-07-20 13:29:08 -0400

    add transformer

--- transformer.py
+++ transformer.py
@@ -11,6 +11,7 @@ class Transformer(ForcastBaseModel):
         embedding_dim,
         nhead,
         hidden_size=100,
+        num_layers=1,
         model_save_path="./transformer_models",
         feature_type="sequentials",
         label_type="next_log", # might want to change this (anomaly detection)
@@ -32,6 +33,51 @@ class Transformer(ForcastBaseModel):
             embedding_dim=embedding_dim,
             freeze=freeze,
             gpu=gpu,
-
         )
+        num_labels = meta_data["num_labels"]
+        self.feature_type = feature_type
+        self.hiddem_size = hidden_size
+        self.use_tfidf = use_tfidf
+
+        self.cls = torch.zeros(1, 1, embedding_dim).to(self.device)
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=embedding_dim,
+            nhead=nhead,
+            dim_feedforward=hidden_size,
+        )
+
+        self.transformer_encoder = nn.TransformerEncoder(
+            encoder_layer, num_layers=num_layers
+        )
+
+        self.criterion = nn.CrossEntropyLoss()
+
+        self.prediction_layer = nn.Linear(embedding_dim, num_labels)
+    
+    def forward(self, input_dict):
+        if self.label_type == "anomaly":
+            y = input_dict["window_anomalies"].long().view(-1)
+        elif self.label_type == "next_log":
+            y = input_dict["window_labels"].long().view(-1)
+        
+        self.batch_size = y.size()[0]
+        x = input_dict["features"]
+        x = self.embedder(x)
+
+        if self.feature_type == "semantics" and not self.use_tfidf:
+            # sum the token embeddings in the sequence
+            x = x.sum(dim=-2)
+        
+        x_t = x.transpose(1, 0)
+        x_transformed = self.transformer_encoder(x_t.float())
+        representation = x_transformed.transpose(1, 0).mean(dim=1)
+
+        logits = self.prediction_layer(representation)
+        y_pred = logits.softmax(dim=-1)
+
+        loss = self.criterion(logits, y)
+
+        return_dict = {"loss" : loss, "y_pred": y_pred, "y": y}
+
+        return return_dict
         
